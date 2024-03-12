5d080d2c9f4c46775bc574859021d4391e899e33
[enter a username between the quotes - your GitHub username works but is not required] <122652119+vitordelfrari@users.noreply.github.com>
2024-03-12 20:07:39 -0300

    test

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -2,30 +2,30 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&units=imperial&appid=[9764c87cf255c860a4c4713be4f4dc97]';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=[9764c87cf255c860a4c4713be4f4dc97]&units=imperial';
 
-async function apiFetch() {
-    try {
+async function apiFetch(){
+    try{
         const response = await fetch(url);
-        if (response.ok) {
+        if(response.ok){
             const data = await response.json();
-            console.log(data); // testing only
-            // displayResults(data); // uncomment when ready
+            console.log(data); //Testing only
+            displayResults(data); 
         } else {
             throw Error(await response.text());
         }
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
   
-apiFetch();
-
 function displayResults(data) {
-    currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    let desc = data.weather[0].description.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src",iconsrc);
+    weatherIcon.setAttribute("alt","weather icon");
+    captionDesc.textContent =`${desc}`;
 }
+
+apiFetch();
