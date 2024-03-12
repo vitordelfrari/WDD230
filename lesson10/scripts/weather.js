b450b07909ee367b52bc76f991fd3cf9360925a1
[enter a username between the quotes - your GitHub username works but is not required] <122652119+vitordelfrari@users.noreply.github.com>
2024-03-12 20:18:34 -0300

    Test

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=[9764c87cf255c860a4c4713be4f4dc97]&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=9764c87cf255c860a4c4713be4f4dc97&units=imperial';
 
 async function apiFetch(){
     try{
